4eab5ae201f5b164b74ae61bfeee1ec6c058cd09
Vladyslav <vladyslav.oliynyk.2000@gmail.com>
2022-12-04 19:12:03 +0200

    feature: create ID array of hovered cells

--- src/components/modeHandler.js
+++ src/components/modeHandler.js
@@ -2,19 +2,27 @@ import React from "react";
 
 export default function ModeHandler( {mode}) {
 
-    const ROW = [];
-    for (let i=1; i<=mode; i++) {
-        ROW.push(<td id={i.toString()}>  </td>)
-    }
+    const log = [];
+    let rowID;
+
+const handleMouseEnterRow = event => { rowID = event.currentTarget.id; }
+
+    const handleMouseEnterColumn = event => { log.push([event.currentTarget.id, rowID]); console.log(log)   }
+
 
     const COLUMN = [];
     for (let i=1; i<=mode; i++) {
-        COLUMN.push(<tr id={i.toString()}> {ROW} </tr>)
+        COLUMN.push(<td onMouseEnter={handleMouseEnterColumn} id={i.toString()}>  </td>)
+    }
+
+    const ROW = [];
+    for (let i=1; i<=mode; i++) {
+        ROW.push(<tr onMouseEnter={handleMouseEnterRow} id={i.toString()}> {COLUMN} </tr>)
     }
 
 return(
     <table>
-        {COLUMN}
+        {ROW}
     </table>
 )
 
