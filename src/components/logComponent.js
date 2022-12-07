e555ae0cc6b12a3f7c1c83905321c4b6fb793c8b
Vladyslav <vladyslav.oliynyk.2000@gmail.com>
2022-12-07 17:38:19 +0200

    markup: add log window

--- src/components/logComponent.js
+++ src/components/logComponent.js
@@ -1,16 +1,20 @@
 import React, {useRef} from "react";
-
+import s from "./logComponent.module.css"
 
 export default function LogComponent( {getLogID}) {
     const logRef = useRef([])
     logRef.current[logRef.current.length-1] !== getLogID && logRef.current.push(getLogID)
         console.log(logRef)
+    console.log(getLogID)
     const logs = logRef.current
-    const logElement = logs.map((log) => <div><p>hit coordinates col {log[0]} row {log[1]}  </p></div>).reverse()
+    logs[0]==[''] && logs.splice(0)
+    console.log(logs)
+    const logElement = logs.map((log) =>   <div><p>hit coordinates col {log[0]} row {log[1]}  </p></div>).reverse()
+    const logList =<div className={s.logBox}>{logElement}</div>
 
     return(
-        <div>
-            {logElement}
-        </div>
+         <>
+             {logs && logList}
+         </>
     )
 }
