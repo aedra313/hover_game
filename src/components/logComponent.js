b00e794f53076f105bc0945086acc87b49d9b6f1
Vladislav Institoris <vladislavinstitoris@Vladislavs-MacBook.local>
2022-12-14 10:47:56 +0200

    bugfix: log didnt show row value && string with no values

--- src/components/logComponent.js
+++ src/components/logComponent.js
@@ -3,13 +3,21 @@ import s from "./logComponent.module.css"
 
 export default function LogComponent( {getLogID}) {
     const logRef = useRef([])
-    logRef.current[logRef.current.length-1] !== getLogID && logRef.current.push(getLogID)
-        console.log(logRef)
+    logRef.current[logRef.current.length - 1] !== getLogID && logRef.current.push(getLogID)
+    console.log(logRef)
+
     console.log(getLogID)
     const logs = logRef.current
-    logs[0]==[''] && logs.splice(0)
+
+
+    if(logs.length >= 2 && logs[logs.length - 1][1] === undefined){
+        logs[logs.length - 1][1] = logs[logs.length - 2][1]
+    }
+
+
+    logs[0]===[''] && logs.splice(0)
     console.log(logs)
-    const logElement = logs.map((log) =>   <div><p>hit coordinates col {log[0]} row {log[1]}  </p></div>).reverse()
+    const logElement = logs.map((log) =>   <div><p className={s.logItem}>hit coordinates col {log[0]} row {log[1]}  </p></div>).reverse()
     const logList =<div className={s.logBox}>{logElement}</div>
 
     return(
